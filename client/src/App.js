3885f83a58e6fac05ae2fe9441e7899ab4f14bf1
ryan-avancena <ravancena.dev@gmail.com>
2024-03-27 14:11:51 -0700

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,30 @@
+import React, {useState, useEffect} from 'react';
+import Header from './components/Header/Header'
+import SearchAndMap from './components/SearchAndMap/SearchMap'
+import './App.css'
+
+
+const App = () => {
+    const [clickedLatLng, setClickedLatLng] = useState(null);
+    const [nRadius, setRadius] = useState(null);
+
+    const handleLatLngChange = (latLng) => {
+        console.log("New clickedLatLng:", latLng);
+        setClickedLatLng(latLng);
+    };
+
+    const handleRadiusChange = (nRadius) => {
+        console.log("radius (App.js): ", nRadius)
+        setRadius(nRadius)
+    }   
+    
+
+    return (
+        <div className='body'>
+            <Header/>
+            <SearchAndMap/>
+        </div> 
+    )
+}
+
+export default App;
